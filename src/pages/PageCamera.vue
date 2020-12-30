46104ab4163d32544f6615c61b673c16caaf144e
Rolland Orgonas <rorgonas@gmail.com>
2020-12-29 21:38:40 -0500

    quasar - camera - check camera support & capture-upload image

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -16,12 +16,25 @@
     </div>
     <div class="text-center q-pa-md">
       <q-btn
+        v-if="hasCameraSupport"
         color="grey-10"
         icon="eva-camera"
         size="lg"
         round
         @click="captureImage"
       />
+      <q-file
+        v-else
+        outlined
+        @input="captureImageFallback"
+        v-model="imageUpload"
+        accept="image/*"
+        label="Choose an image"
+      >
+        <template v-slot:prepend>
+          <q-icon name="eva-attach-outline" />
+        </template>
+      </q-file>
       <div class="row justify-center q-ma-md">
         <q-input
           v-model="post.caption"
@@ -64,7 +77,9 @@ export default {
         photo: null,
         date: Date.now()
       },
-      imageCaptured: false
+      imageCaptured: false,
+      imageUpload: [],
+      hasCameraSupport: true
     }
   },
   methods: {
@@ -77,7 +92,7 @@ export default {
         stream = await navigator.mediaDevices.getUserMedia(constraints);
         this.$refs.video.srcObject = stream
       } catch(err) {
-        /* handle the error */
+        this.hasCameraSupport = false
       }
     },
     captureImage() {
@@ -88,10 +103,64 @@ export default {
       let context = canvas.getContext('2d')
       context.drawImage(video, 0,0, canvas.width, canvas.height)
       this.imageCaptured = true
+      this.post.photo = this.dataURItoBlob(canvas.toDataURL())
+      this.disableCamera()
+    },
+    captureImageFallback(file) {
+      this.post.photo = file
+      const reader = new FileReader();
+      let canvas = this.$refs.canvas
+      const context = canvas.getContext('2d');
+
+      // Upload image to canvas
+      reader.onload = (event) => {
+        const img = new Image();
+        img.onload = () => {
+          canvas.width = img.width
+          canvas.height = img.height
+          context.drawImage(img,0,0)
+          this.imageCaptured = true
+        }
+        img.src = event.target.result
+      }
+      reader.readAsDataURL(file)
+    },
+    // Transform data URL to Blob image
+    dataURItoBlob(dataURI) {
+      // convert base64 to raw binary data held in a string
+      // doesn't handle URLEncoded DataURIs - see SO answer #6850276 for code that does this
+      const byteString = atob(dataURI.split(',')[1]);
+
+      // separate out the mime component
+      const mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+
+      // write the bytes of the string to an ArrayBuffer
+      const ab = new ArrayBuffer(byteString.length);
+
+      // create a view into the buffer
+      const ia = new Uint8Array(ab);
+
+      // set the bytes of the buffer to the correct values
+      for (let i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+      }
+
+      // write the ArrayBuffer to a blob, and you're done
+      return new Blob([ab], {type: mimeString});
+    },
+    disableCamera() {
+      this.$refs.video.srcObject.getVideoTracks().forEach(track => {
+        track.stop()
+      })
     }
   },
   mounted() {
     this.initCamera()
+  },
+  beforeDestroy() {
+    if(this.hasCameraSupport) {
+      this.disableCamera()
+    }
   }
 }
 </script>
