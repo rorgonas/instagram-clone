1a8c1f7c72537d927be146a0e9628d24daa1b691
Rolland Orgonas <rorgonas@gmail.com>
2020-12-29 19:56:25 -0500

    quasar - responsive - post layout

--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,33 +1,54 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="card-post q-mb-md"
-      flat
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-          </q-avatar>
-        </q-item-section>
 
-        <q-item-section>
-          <q-item-label class="text-bold">rolland_orgonas</q-item-label>
-          <q-item-label caption>
-            {{ post.location }}
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-separator />
-      <q-img :src="post.imageUrl" />
-      <q-card-section>
-        <div>{{ post.caption }}</div>
-        <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
-      </q-card-section>
-    </q-card>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">rolland_orgonas</q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date | niceDate }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <q-img src="https://cdn.quasar.dev/img/boy-avatar.png"/>
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">rolland_orgonas</q-item-label>
+            <q-item-label caption>
+              Rolland Orgonas
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -72,7 +93,7 @@ export default {
   },
   filters: {
     niceDate(value) {
-      return date.formatDate(value, 'MMMM D h:mm A')
+      return date.formatDate(value, 'MMMM D h:mmA')
     }
   }
 }
