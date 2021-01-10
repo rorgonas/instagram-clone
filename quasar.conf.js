6603d20dba5560c85818e758cf769cd8a9405b96
Rolland Orgonas <rorgonas@gmail.com>
2021-01-10 10:02:19 -0500

    quasar - generate - new icon set

--- quasar.conf.js
+++ quasar.conf.js
@@ -115,13 +115,13 @@ module.exports = function (ctx) {
       workboxPluginMode: 'GenerateSW', // 'GenerateSW' or 'InjectManifest'
       workboxOptions: {}, // only for GenerateSW
       manifest: {
-        name: `Quasar App`,
+        name: `Quasar App - Instagram Clone`,
         short_name: `Quasar App`,
-        description: `A Quasar Framework app`,
+        description: `A Simple Instagram Clone - Made with Quasar`,
         display: 'standalone',
         orientation: 'portrait',
         background_color: '#ffffff',
-        theme_color: '#027be3',
+        theme_color: '#212121',
         icons: [
           {
             src: 'icons/icon-128x128.png',
