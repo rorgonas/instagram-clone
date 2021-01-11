5fc77017393460d4aee62fb0d41e1462a2d8a91e
Rolland Orgonas <rorgonas@gmail.com>
2021-01-10 22:23:09 -0500

    quasar - cache strategie - implement cacheFirst for google fonts

--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -11,6 +11,9 @@
   import { precacheAndRoute } from 'workbox-precaching';
   import { registerRoute } from 'workbox-routing';
   import { StaleWhileRevalidate } from 'workbox-strategies';
+  import { CacheFirst } from 'workbox-strategies';
+  import { ExpirationPlugin } from 'workbox-expiration';
+  import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 
 /*
 * Config
@@ -25,6 +28,22 @@ precacheAndRoute(self.__WB_MANIFEST);
 * */
 
 registerRoute(
-  ({url}) => url.href.startsWith('http'),
+  ({ url }) => url.host.startsWith('fonts.g'),
+  //Google fonts not rendering when fully offline (see solution: https://github.com/GoogleChrome/workbox/issues/1563)
+  new CacheFirst({
+    cacheName: 'google-fonts',
+    plugins: [
+      new ExpirationPlugin({
+        maxEntries: 30,
+      }),
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      })
+    ],
+  })
+);
+
+registerRoute(
+  ({ url }) => url.href.startsWith('http'),
   new StaleWhileRevalidate()
 );
