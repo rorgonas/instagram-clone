86ef8710e8ecf651220645fdb23b2543914258db
Rolland Orgonas <rorgonas@gmail.com>
2021-01-30 14:58:52 -0500

    quasar - client - enable dependency code transpile for idb lib

--- quasar.conf.js
+++ quasar.conf.js
@@ -55,7 +55,7 @@ module.exports = function (ctx) {
       // Add dependencies for transpiling with Babel (Array of string/regex)
       // (from node_modules, which are by default not transpiled).
       // Applies only if "transpile" is set to true.
-      // transpileDependencies: [],
+      transpileDependencies: ['idb'],
 
       // rtl: false, // https://quasar.dev/options/rtl-support
       // preloadChunks: true,
